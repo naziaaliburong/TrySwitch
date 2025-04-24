4ee23af559e7e45714ebf15b27ed8cd0006ad967
Nazia <naziaaliburong@gmail.com>
2025-04-24 10:18:26 +0500

    hero animation with gsap

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,49 +1,33 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import StoreButtons from './StoreButtons';
+'use client'
+import React, { useEffect, useState, useRef } from 'react'
+import { gsap } from 'gsap'
+import StoreButtons from './StoreButtons'
 
 const Hero = () => {
-  const noOfFrames = 151;
-  const [currentFrame, setCurrentFrame] = useState(0);
-  const [animationComplete, setAnimationComplete] = useState(false);
-  const [preloaded, setPreloaded] = useState(false);
+  const noOfFrames = 151
+  const [currentFrame, setCurrentFrame] = useState(0)
+  const [animationComplete, setAnimationComplete] = useState(false)
+  const frameRef = useRef({ frame: 0 })
 
-  // Preload images
   useEffect(() => {
-    let loadedCount = 0;
+    const tl = gsap.timeline({
+      onComplete: () => setAnimationComplete(true),
+    })
 
-    for (let i = 0; i < noOfFrames; i++) {
-      const frameName = i.toString().padStart(4, '0');
-      const img = new Image();
-      img.src = `/assets/images/${frameName}.png`;
-      img.onload = () => {
-        loadedCount++;
-        if (loadedCount === noOfFrames) {
-          setPreloaded(true); // all images are preloaded
-        }
-      };
-    }
-  }, []);
+    tl.to(frameRef.current, {
+      frame: noOfFrames - 1,
+      duration: (noOfFrames * 40) / 1000, // 40ms per frame
+      ease: 'none',
+      onUpdate: () => {
+        setCurrentFrame(Math.round(frameRef.current.frame))
+      },
+    })
+  }, [])
 
-  // Run animation after preloading
-  useEffect(() => {
-    if (!preloaded) return;
-
-    for (let i = 0; i < noOfFrames; i++) {
-      setTimeout(() => {
-        setCurrentFrame(i);
-      }, i * 40);
-    }
-
-    setTimeout(() => {
-      setAnimationComplete(true);
-    }, noOfFrames * 40);
-  }, [preloaded]);
-
-  const frameName = currentFrame.toString().padStart(4, '0');
+  const frameName = currentFrame.toString().padStart(4, '0')
   const imgSrc = animationComplete
     ? '/assets/images/Tryswitch-hero.png'
-    : `/assets/images/${frameName}.png`;
+    : `/assets/images/${frameName}.png`
 
   return (
     <div className='flex max-md:flex-col bg-gradient-to-r from-[#FFC2AE80] to-[#B7A8FF80] rounded-2xl my-20 relative h-[550px] max-md:h-[750px] max-lg:h-[500px] max-sm:h-[550px] max-lg:mb-35'>
@@ -58,12 +42,12 @@ const Hero = () => {
           src={imgSrc}
           width={300}
           height={300}
-          alt="mobile"
+          alt='mobile'
           className='absolute top-10 left-10 max-lg:left-0 w-full h-full max-md:w-[60%] max-md:ml-[20%]'
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
